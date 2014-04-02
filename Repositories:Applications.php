db0fd7628804c13f84bf6e2975be79fe24c9230f
falkTX <falktx@gmail.com>
2014-04-02 16:18:33 +0100

    More apps & plugins

--- Repositories:Applications.php
+++ Repositories:Applications.php
@@ -6,15 +6,6 @@ $PAGE_SOURCE_2 = ARRAY("Repositories", "Applications");
 include_once("includes/header.php");
 ?>
 
-<!--
-  TODO:
-  - tracktion
-  - paulstretch
-  - arpage + zonage
-  - lmms
-  - jalv?
--->
-
 <div class="box box-description">
     <p>
         This section lists the applications available in the repositories.<br/>
@@ -30,19 +21,17 @@ include_once("includes/header.php");
     <br/>
 </div>
 
-<!-- Aria Maestosa -->
+<!-- AMS -->
 <table>
     <tr><td align="center">
-        <p><a href="<?php echo $ROOT; ?>/screenshots/ariamaestosa.jpg">
-           <img src="screenshots/thumb/ariamaestosa.jpg" alt="ariamaestosa" class="img_thumb_150"/>
+        <p><a href="<?php echo $ROOT; ?>/screenshots/ams.png">
+           <img src="screenshots/thumb/ams.png" alt="ams" class="img_thumb_150"/>
         </a></p>
     </td><td>
         <p>
-            <b><a href="apt://ariamaestosa" style="color: rgb(230,230,100);">Aria Maestosa</a></b> (MIDI Sequencer)<br/>
-            Aria Maestosa is an open-source midi sequencer/editor.<br/>
-            It lets you compose, edit and play midi files with a few clicks in a user-friendly interface offering score, keyboard, guitar, drum and controller views.<br/>
-            Homepage: <a href="http://ariamaestosa.sourceforge.net//" class="external text" rel="nofollow" target="_blank">http://ariamaestosa.sourceforge.net/</a><br/>
-            User Manual: <a href="http://ariamaestosa.sourceforge.net/man.html" class="external text" rel="nofollow" target="_blank">http://ariamaestosa.sourceforge.net/man.html</a><br/>
+            <b><a href="apt://ams" style="color: rgb(230,230,100);">AMS</a></b> (Modular Plugin Host)<br/>
+            AlsaModularSynth is a realtime modular synthesizer and effect processor.<br/>
+            Homepage: <a href="http://alsamodular.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://alsamodular.sourceforge.net/</a><br/>
         </p>
     </td></tr>
 </table>
@@ -64,6 +53,23 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- Aria Maestosa -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/ariamaestosa.jpg">
+           <img src="screenshots/thumb/ariamaestosa.jpg" alt="ariamaestosa" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://ariamaestosa" style="color: rgb(230,230,100);">Aria Maestosa</a></b> (MIDI Sequencer)<br/>
+            Aria Maestosa is an open-source midi sequencer/editor.<br/>
+            It lets you compose, edit and play midi files with a few clicks in a user-friendly interface offering score, keyboard, guitar, drum and controller views.<br/>
+            Homepage: <a href="http://ariamaestosa.sourceforge.net//" class="external text" rel="nofollow" target="_blank">http://ariamaestosa.sourceforge.net/</a><br/>
+            User Manual: <a href="http://ariamaestosa.sourceforge.net/man.html" class="external text" rel="nofollow" target="_blank">http://ariamaestosa.sourceforge.net/man.html</a><br/>
+        </p>
+    </td></tr>
+</table>
+
 <!-- Bitwig Studio -->
 <table>
     <tr><td align="center">
@@ -72,7 +78,7 @@ include_once("includes/header.php");
         </a></p>
     </td><td>
         <p>
-            <b><a href="apt://bitwig-studio" style="color: rgb(230,230,100);">Bitwig Studio</a></b> (DAW)<br/>
+            <b><a href="apt://bitwig-studio" style="color: rgb(230,230,100);">Bitwig Studio</a></b> (DAW, non-free)<br/>
             Bitwig Studio is a multi-platform music-creation system for production, remixing and performance<br/>
             with a focus on flexible editing tools and a super-fast workflow.<br/>
             Homepage: <a href="http://www.bitwig.com/" class="external text" rel="nofollow" target="_blank">http://www.bitwig.com/</a><br/>
@@ -104,7 +110,7 @@ include_once("includes/header.php");
         </a></p>
     </td><td>
         <p>
-            <b><a href="apt://carla" style="color: rgb(230,230,100);">Carla</a></b> (Plugin Host)<br/>
+            <b><a href="apt://carla" style="color: rgb(230,230,100);">Carla</a></b> (Modular Plugin Host)<br/>
             Carla is an audio plugin host, with support for many audio drivers and plugin formats.<br/>
             It has some nice features like automation of parameters via MIDI CC and full OSC control.<br/>
             Currently supports LADSPA, DSSI, LV2, and VST plugin formats, and GIG, SF2 and SFZ sounds banks.<br/>
@@ -146,7 +152,7 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
-<!-- LinuxSampler -->
+<!-- jalv -->
 <table>
     <tr><td align="center">
         <p>
@@ -154,10 +160,28 @@ include_once("includes/header.php");
         </p>
     </td><td>
         <p>
-            <b><a href="apt://linuxsampler" style="color: rgb(230,230,100);">LinuxSampler</a></b> (Sampler)<br/>
-            Free, streaming capable open source pure software audio sampler with professional grade features,<br/>
-            comparable to both hardware and commercial Windows/Mac software samplers.<br/>
-            Homepage and Documentation: <a href="http://www.linuxsampler.org/" class="external text" rel="nofollow" target="_blank">http://www.linuxsampler.org/</a><br/>
+            <b><a href="apt://jalv" style="color: rgb(230,230,100);">jalv</a></b> (Plugin Host)<br/>
+            Jalv is a simple but fully featured LV2 host for Jack.<br/>
+            It runs LV2 plugins and exposes their ports as Jack ports, essentially making any LV2 plugin function as a Jack application.<br/>
+            Homepage: <a href="http://drobilla.net/software/jalv/" class="external text" rel="nofollow" target="_blank">http://drobilla.net/software/jalv/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- LMMS -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/lmms.png">
+           <img src="screenshots/thumb/lmms.png" alt="lmms" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://lmms" style="color: rgb(230,230,100);">LMMS</a></b> (DAW) [*]<br/>
+            LMMS combines the features of a tracker-/sequencer-program (pattern-/channel-/sample-/song-/effect-management)<br/>
+            and those of powerful synthesizers and samplers in a modern, user-friendly and easy to use graphical user-interface.<br/>
+            Homepage: <a href="http://lmms.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://lmms.sourceforge.net/</a><br/>
+            Wiki: <a href="http://lmms.sourceforge.net/wiki/index.php/Main_Page" class="external text" rel="nofollow" target="_blank">http://lmms.sourceforge.net/wiki/index.php/Main_Page</a><br/>
+            [*] Ubuntu only, for now.<br/>
         </p>
     </td></tr>
 </table>
@@ -173,7 +197,7 @@ include_once("includes/header.php");
             <b><a href="apt://luppp" style="color: rgb(230,230,100);">Luppp</a></b> (Live Looper)<br/>
             Luppp is a music creation tool, intended for live use.<br/>
             It focuses on real time processing and a fast intuitive workflow.<br/>
-            Homepage and Documentation: <a href="http://openavproductions.com/luppp/" class="external text" rel="nofollow" target="_blank">http://openavproductions.com/luppp/</a><br/>
+            Homepage and Tutorials: <a href="http://openavproductions.com/luppp/" class="external text" rel="nofollow" target="_blank">http://openavproductions.com/luppp/</a><br/>
         </p>
     </td></tr>
 </table>
@@ -231,6 +255,24 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- PaulStretch -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/paulstretch.png">
+           <img src="screenshots/thumb/paulstretch.png" alt="paulstretch" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://paulstretch" style="color: rgb(230,230,100);">PaulStretch</a></b> (Audio Stretch) [*]<br/>
+            PaulStretch is a program for stretching the audio.<br/>
+            It is suitable only for extreme sound stretching of the audio (like 50x) and for applying special effects by "spectral smoothing" the sounds.<br/>
+            Homepage and Documentation: <a href="http://hypermammut.sourceforge.net/paulstretch/" class="external text" rel="nofollow" target="_blank">http://hypermammut.sourceforge.net/paulstretch/</a><br/>
+            [*] This is a custom version that uses NTK, see
+              <a href="https://github.com/original-male/paulstretch_cpp" class="external text" rel="nofollow" target="_blank">this link</a>.<br/>
+        </p>
+    </td></tr>
+</table>
+
 <!-- ProTrekkr -->
 <table>
     <tr><td align="center">
@@ -243,10 +285,10 @@ include_once("includes/header.php");
             Tracker program combining a software synthesizer together with a traditional samples tracker<br/>
             which can be used to create electronic music (like psytrance, trance goa, hard acid, IDM, chip, techno, etc)<br/>
             for small sized intros, demos or games.<br/>
-            Homepage: <a href="http://code.google.com/p/protrekkr/" class="external text" rel="nofollow" target="_blank">http://code.google.com/p/protrekkr/</a><br/>
+            Project Page: <a href="http://code.google.com/p/protrekkr/" class="external text" rel="nofollow" target="_blank">http://code.google.com/p/protrekkr/</a><br/>
             Wiki: <a href="http://code.google.com/p/protrekkr/wiki/introduction?tm=6" class="external text" rel="nofollow" target="_blank">http://code.google.com/p/protrekkr/wiki/introduction?tm=6</a><br/>
             [*] This is a custom version with JACK audio support, see
-              <a href="https://github.com/falkTX/protrekkr" class="external text" rel="nofollow" target="_blank">this link</a>.
+              <a href="https://github.com/falkTX/protrekkr" class="external text" rel="nofollow" target="_blank">this link</a>.<br/>
         </p>
     </td></tr>
 </table>
@@ -331,6 +373,21 @@ include_once("includes/header.php");
             It is a tool for those people who like to compose music wherever they are, whenever they wish.<br/>
             Homepage: <a href="http://www.warmplace.ru/soft/sunvox/" class="external text" rel="nofollow" target="_blank">http://www.warmplace.ru/soft/sunvox/</a><br/>
             User Manual: <a href="http://www.warmplace.ru/wiki/sunvox:manual_en" class="external text" rel="nofollow" target="_blank">http://www.warmplace.ru/wiki/sunvox:manual_en</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- Tracktion -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/tracktion.png">
+           <img src="screenshots/thumb/tracktion.png" alt="tracktion" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://tracktion" style="color: rgb(230,230,100);">Tracktion</a></b> (DAW, non-free)<br/>
+            Tracktion is a powerful workstation for audio/midi recording and editing.<br/>
+            Homepage: <a href="http://tracktion.com/" class="external text" rel="nofollow" target="_blank">http://tracktion.com/</a><br/>
         </p>
     </td></tr>
 </table>
--- Repositories:Plugins.php
+++ Repositories:Plugins.php
@@ -23,26 +23,32 @@ include_once("includes/header.php");
 
 <!--
   TODO:
-    avw.lv2
-    drmr
-    drumgizmo
-    eq10q
     hexter
-    holap
+    oxbd
     phasex
-    triceratops-lv2
-    pizmidi-plugins
+    spiralsynthmodular
+    teragon-audio
     whysynth
     wsynth-dssi
     zynaddsubfx
-    teragon-audio
-    oxbd
-    artyfx
-    fabla
-    sorcer
-    spiralsynthmodular
-    ams
  -->
+
+<!-- XXXpp -->
+<!--
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/XXX.png">
+           <img src="screenshots/thumb/XXX.png" alt="XXX" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://XXX" style="color: rgb(100,230,100);">XXX</a></b> (Standalone)<br/>
+            <br/>
+            Homepage: <a href="WWW" class="external text" rel="nofollow" target="_blank">WWW</a><br/>
+        </p>
+    </td></tr>
+</table>
+-->
 
 <!-- see later the non-free stuff, pianoteq, loomer, linuxdsp, etc -->
 
@@ -77,6 +83,37 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- Arpage + Zonage -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/arpage.png">
+           <img src="screenshots/thumb/arpage.png" alt="arpage" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://arpage" style="color: rgb(100,230,100);">Arpage + Zonage</a></b> (Standalone)<br/>
+            JACK MIDI arpeggiator with transport and tempo sync.<br/>
+            Project Page: <a href="http://sourceforge.net/projects/arpage/" class="external text" rel="nofollow" target="_blank">http://sourceforge.net/projects/arpage/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- Arty FX -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/artyfx.png">
+           <img src="screenshots/thumb/artyfx.png" alt="artyfx" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://artyfx" style="color: rgb(100,230,100);">Arty FX</a></b> (LV2)<br/>
+            Arty FX is a plugin bundle of artistic “RT” effects.<br/>
+            The aim is to allow the designing of your sound just as you desired using a fast, effiecient workflow.<br/>
+            Homepage: <a href="http://openavproductions.com/artyfx/" class="external text" rel="nofollow" target="_blank">http://openavproductions.com/artyfx/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
 <!-- Calf -->
 <table>
     <tr><td align="center">
@@ -89,7 +126,7 @@ include_once("includes/header.php");
              The Calf plugins include all todays frequently used studio effects along with some feature-rich sound processors.<br/>
             Homepage: <a href="http://calf.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://calf.sourceforge.net/</a><br/>
             [*] This is a custom version which adds back LADSPA, DSSI and LV2 external-UI support removed by upstream, see
-              <a href="https://github.com/falkTX/calf" class="external text" rel="nofollow" target="_blank">this link</a>.
+              <a href="https://github.com/falkTX/calf" class="external text" rel="nofollow" target="_blank">this link</a>.<br/>
         </p>
     </td></tr>
 </table>
@@ -110,6 +147,38 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- drmr -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/drmr.png">
+           <img src="screenshots/thumb/drmr.png" alt="drmr" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://drmr" style="color: rgb(100,230,100);">drmr</a></b> (LV2)<br/>
+            DrMr is an LV2 sampler plugin.<br/>
+            It  gives a way for lv2 hosts to have a built in drum synth that can save its entire state.<br/>
+            Project Page: <a href="https://github.com/nicklan/drmr/wiki" class="external text" rel="nofollow" target="_blank">https://github.com/nicklan/drmr/wiki</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- DrumGizmo -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/drumgizmo.png">
+           <img src="screenshots/thumb/drumgizmo.png" alt="drumgizmo" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://drumgizmo" style="color: rgb(100,230,100);">DrumGizmo</a></b> (Standalone + LV2)<br/>
+            DrumGizmo is a cross-platform drum plugin and stand-alone application.<br/>
+            It is comparable to several commercial drum plugin products.<br/>
+            Homepage/Wiki: <a href="http://www.drumgizmo.org/wiki/" class="external text" rel="nofollow" target="_blank">http://www.drumgizmo.org/wiki/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
 <!-- drumkv1 -->
 <table>
     <tr><td align="center">
@@ -121,6 +190,38 @@ include_once("includes/header.php");
             <b><a href="apt://drumkv1" style="color: rgb(100,230,100);">drumkv1</a></b> (Standalone + LV2)<br/>
             drumkv1 is an old-school all-digital drum-kit sampler synthesizer with stereo fx.<br/>
             Homepage: <a href="http://drumkv1.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://drumkv1.sourceforge.net/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- EQ10Q -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/eq10q.png">
+           <img src="screenshots/thumb/eq10q.png" alt="eq10q" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://eq10q" style="color: rgb(100,230,100);">EQ10Q</a></b> (LV2)<br/>
+            EQ10Q is a powerfull and flexible parametric equalizer.<br/>
+            It includes parametric equalization with diferent filter types like peaking, HPF, LPF, Shelving and Notch with IIR algorithms<br/>
+            and a nice GUI displaying the equalization curve.<br/>
+            Homepage: <a href="http://eq10q.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://eq10q.sourceforge.net/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- Fabla -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/fabla.png">
+           <img src="screenshots/thumb/fabla.png" alt="fabla" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://fabla" style="color: rgb(100,230,100);">Fabla</a></b> (LV2)<br/>
+            Fabla is a drum sampler plugin ideal for loading up your favorite sampled sounds and bashing away on a MIDI controller.<br/>
+            Homepage: <a href="http://openavproductions.com/fabla/" class="external text" rel="nofollow" target="_blank">http://openavproductions.com/fabla/</a><br/>
         </p>
     </td></tr>
 </table>
@@ -141,7 +242,23 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
-<!-- LinuxSampler (DSSI) -->
+<!-- HOLAP -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/horgand.png">
+           <img src="screenshots/thumb/horgand.png" alt="horgand" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://holap" style="color: rgb(100,230,100);">HOLAP</a></b> (LADSPA + DSSI)<br/>
+            HOLAP is a set of DSSI and LADSPA audio plugins including DSP effects and a FM synthesizer.<br/>
+            Most of this LADSPA plugins are based on the wonderful ZynAddSubFX synthesizer (Paul Nasca) set of effects.<br/>
+            Homepage: <a href="http://holap.berlios.de/" class="external text" rel="nofollow" target="_blank">http://holap.berlios.de/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- LinuxSampler -->
 <table>
     <tr><td align="center">
         <p>
@@ -149,7 +266,7 @@ include_once("includes/header.php");
         </p>
     </td><td>
         <p>
-            <b><a href="apt://linuxsampler-dssi" style="color: rgb(100,230,100);">LinuxSampler</a></b> (DSSI)<br/>
+            <b><a href="apt://linuxsampler-all" style="color: rgb(100,230,100);">LinuxSampler</a></b> (Standalone + DSSI + LV2 + VST)<br/>
             Free, streaming capable open source pure software audio sampler with professional grade features,<br/>
             comparable to both hardware and commercial Windows/Mac software samplers.<br/>
             Homepage and Documentation: <a href="http://www.linuxsampler.org/" class="external text" rel="nofollow" target="_blank">http://www.linuxsampler.org/</a><br/>
@@ -157,54 +274,7 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
-<!-- LinuxSampler (LV2) -->
-<table>
-    <tr><td align="center">
-        <p>
-           <img src="screenshots/thumb/nogui.png" alt="nogui" class="img_thumb_150"/>
-        </p>
-    </td><td>
-        <p>
-            <b><a href="apt://linuxsampler-lv2" style="color: rgb(100,230,100);">LinuxSampler</a></b> (LV2, 2 channels)<br/>
-            Free, streaming capable open source pure software audio sampler with professional grade features,<br/>
-            comparable to both hardware and commercial Windows/Mac software samplers.<br/>
-            Homepage and Documentation: <a href="http://www.linuxsampler.org/" class="external text" rel="nofollow" target="_blank">http://www.linuxsampler.org/</a><br/>
-        </p>
-    </td></tr>
-</table>
-
-<!-- LinuxSampler (LV2, 32chan) -->
-<table>
-    <tr><td align="center">
-        <p>
-           <img src="screenshots/thumb/nogui.png" alt="nogui" class="img_thumb_150"/>
-        </p>
-    </td><td>
-        <p>
-            <b><a href="apt://linuxsampler-lv2-32chan" style="color: rgb(100,230,100);">LinuxSampler</a></b> (LV2, 32 channels)<br/>
-            Free, streaming capable open source pure software audio sampler with professional grade features,<br/>
-            comparable to both hardware and commercial Windows/Mac software samplers.<br/>
-            Homepage and Documentation: <a href="http://www.linuxsampler.org/" class="external text" rel="nofollow" target="_blank">http://www.linuxsampler.org/</a><br/>
-        </p>
-    </td></tr>
-</table>
-
-<!-- LinuxSampler (VST) -->
-<table>
-    <tr><td align="center">
-        <p>
-           <img src="screenshots/thumb/nogui.png" alt="nogui" class="img_thumb_150"/>
-        </p>
-    </td><td>
-        <p>
-            <b><a href="apt://linuxsampler-vst" style="color: rgb(100,230,100);">LinuxSampler</a></b> (VST)<br/>
-            Free, streaming capable open source pure software audio sampler with professional grade features,<br/>
-            comparable to both hardware and commercial Windows/Mac software samplers.<br/>
-            Homepage and Documentation: <a href="http://www.linuxsampler.org/" class="external text" rel="nofollow" target="_blank">http://www.linuxsampler.org/</a><br/>
-        </p>
-    </td></tr>
-</table>
-
+<!-- MDA (LV2) -->
 <table>
     <tr><td align="center">
         <p>
@@ -220,6 +290,23 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- pizmidi-plugins -->
+<table>
+    <tr><td align="center">
+        <p>
+           <img src="screenshots/thumb/nogui.png" alt="nogui" class="img_thumb_150"/>
+        </p>
+    </td><td>
+        <p>
+            <b><a href="apt://pizmidi-plugins" style="color: rgb(100,230,100);">pizmidi</a></b> (VST)<br/>
+            pizmidi is a collection of cross-platform MIDI processing plugins.<br/>
+            Homepage: <a href="http://thepiz.org/plugins/" class="external text" rel="nofollow" target="_blank">http://thepiz.org/plugins/</a><br/>
+            Project Page: <a href="http://code.google.com/p/pizmidi/" class="external text" rel="nofollow" target="_blank">http://code.google.com/p/pizmidi/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- REV-Plugins -->
 <table>
     <tr><td align="center">
         <p>
@@ -235,6 +322,7 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- samplv1 -->
 <table>
     <tr><td align="center">
         <p><a href="<?php echo $ROOT; ?>/screenshots/samplv1.png">
@@ -249,6 +337,23 @@ include_once("includes/header.php");
     </td></tr>
 </table>
 
+<!-- Sorcer -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/sorcer.png">
+           <img src="screenshots/thumb/sorcer.png" alt="sorcer" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://sorcer" style="color: rgb(100,230,100);">Sorcer</a></b> (LV2)<br/>
+            Sorcer is a polyphonic wavetable synth plugin.<br/>
+            Its sonic fingerprint is one of harsh modulated sub-bass driven walls of sound.<br/>
+            Homepage: <a href="http://openavproductions.com/sorcer/" class="external text" rel="nofollow" target="_blank">http://openavproductions.com/sorcer/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- synthv1 -->
 <table>
     <tr><td align="center">
         <p><a href="<?php echo $ROOT; ?>/screenshots/synthv1.png">
@@ -259,6 +364,21 @@ include_once("includes/header.php");
             <b><a href="apt://synthv1" style="color: rgb(100,230,100);">synthv1</a></b> (Standalone + LV2)<br/>
             synthv1 is an old-school all-digital 4-oscillator subtractive polyphonic synthesizer with stereo fx.<br/>
             Homepage: <a href="http://synthv1.sourceforge.net/" class="external text" rel="nofollow" target="_blank">http://synthv1.sourceforge.net/</a><br/>
+        </p>
+    </td></tr>
+</table>
+
+<!-- triceratops -->
+<table>
+    <tr><td align="center">
+        <p><a href="<?php echo $ROOT; ?>/screenshots/triceratops.png">
+           <img src="screenshots/thumb/triceratops.png" alt="triceratops" class="img_thumb_150"/>
+        </a></p>
+    </td><td>
+        <p>
+            <b><a href="apt://triceratops-lv2" style="color: rgb(100,230,100);">Triceratops</a></b> (LV2)<br/>
+            Triceratops is a polyphonic subtractive synthesizer plugin.<br/>
+            Project Page: <a href="http://sourceforge.net/projects/triceratops/" class="external text" rel="nofollow" target="_blank">http://sourceforge.net/projects/triceratops/</a><br/>
         </p>
     </td></tr>
 </table>
