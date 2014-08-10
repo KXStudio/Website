e5dc1c6b27d824ec6c581f3ab281e0672c54cfd4
falkTX <falktx@gmail.com>
2014-08-10 12:50:30 +0100

    Update downloads

--- Downloads.php
+++ Downloads.php
@@ -12,30 +12,37 @@ include_once("includes/header.php");
         Software developed by the KXStudio Team, pre-compiled and ready to run.<br/>
     </p>
     <p class="p_extra_spaced">
-        Linux Downloads:<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence</a> (32bit, version 0.8.1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence</a> (64bit, version 0.8.1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla</a> (32bit, version 1.2.4)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla</a> (64bit, version 1.2.4)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta1-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2</a> (32bit, version 2.0-beta1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta1-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2</a> (64bit, version 2.0-beta1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/festige/festige-1.0.2-x86.tar.gz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_festige.png" alt="" class="img_text_align"/> FeSTige</a> (32bit, version 1.0.2)<br/>
+        <b>Linux Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence </a> (32bit, version 0.8.1)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence </a> (64bit, version 0.8.1)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla </a> (32bit, version 1.2.4)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla </a> (64bit, version 1.2.4)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2-linux32.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (32bit, version 2.0-beta2)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2-linux64.tar.xz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (64bit, version 2.0-beta2)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/festige/festige-1.0.2-x86.tar.gz/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_festige.png" alt="" class="img_text_align"/> FeSTige </a> (32bit, version 1.0.2)<br/>
     </p>
     <p class="p_extra_spaced">
-        Windows Downloads:<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence</a> (32bit, version 0.8.1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla</a> (32bit, version 1.2.4)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta1-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2</a> (32bit, version 2.0-beta1)<br/>
+        <b>MacOS Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2b-mac.dmg/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (version 2.0-beta2 for 10.6 or higher)<br/>
+    </p>
+    <p class="p_extra_spaced">
+        <b>Windows Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence </a> (32bit, version 0.8.1)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla </a> (32bit, version 1.2.4)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2b-win32.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (32bit, version 2.0-beta2)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2b-win64.zip/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (64bit, version 2.0-beta2)<br/>
     </p>
 </ul>
 
@@ -48,17 +55,15 @@ include_once("includes/header.php");
         If you're running a Debian or Ubuntu based system, please use the <a href="<?php echo $ROOT; ?>/Repositories">KXStudio Repositories</a> instead.
     </p>
     <p class="p_extra_spaced">
-        Downloads:<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence</a> (version 0.8.1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla</a> (version 1.2.4)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta1-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2</a> (version 2.0-beta1)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/festige/festige-1.0.2-src.tar.gz/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_festige.png" alt="" class="img_text_align"/> FeSTige</a> (version 1.0.2)<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/klaudia/klaudia-1.0.2.tar.gz/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_klaudia.png" alt="" class="img_text_align"/> Klaudia</a> (version 1.0.2)<br/>
+        <b>Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/cadence/Cadence-0.8.1-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_cadence.png" alt="" class="img_text_align"/> Cadence </a> (version 0.8.1)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-1.2.4-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla </a> (version 1.2.4)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/carla/Carla-2.0beta2-src.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_carla.png" alt="" class="img_text_align"/> Carla2 </a> (version 2.0-beta2)<br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/festige/festige-1.0.2-src.tar.gz/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_festige.png" alt="" class="img_text_align"/> FeSTige </a> (version 1.0.2)<br/>
     </p>
 </ul>
 
@@ -71,19 +76,17 @@ include_once("includes/header.php");
         It contains a snapshot of the KXStudio features as of <b>May 7th, 2014</b> or <b>07/05/2014</b>. It uses KDE4 as Desktop Environment.
     </p>
     <p class="p_extra_spaced">
-        Downloads:<br/>
+        <b>Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Live/KXStudio_14.04b_32bit.iso/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_disc.png" alt="" class="img_text_align"/> KXStudio 14.04 Live-DVD <strong>- 32bit</strong></a><br/>
+              &nbsp;&nbsp;&nbsp;&nbsp;md5sum: f252eafbc293e04f6e914554957388bb<br/>
+              &nbsp;&nbsp;&nbsp;&nbsp;size: 1.4 GiB
     </p>
     <p>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Live/KXStudio_14.04b_32bit.iso/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_disc.png" alt="" class="img_text_align"/> KXStudio 14.04 Live-DVD <strong>- 32bit</strong></a><br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;md5sum: f252eafbc293e04f6e914554957388bb<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;size: 1.4 GiB
-    </p>
-    <p>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Live/KXStudio_14.04b_64bit.iso/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_disc.png" alt="" class="img_text_align"/> KXStudio 14.04 Live-DVD <strong>- 64bit</strong></a><br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;md5sum: 4954d39a72292b0da911d734e1d661cf<br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;size: 1.5 GiB
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Live/KXStudio_14.04b_64bit.iso/download" class="external text a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_disc.png" alt="" class="img_text_align"/> KXStudio 14.04 Live-DVD <strong>- 64bit</strong></a><br/>
+              &nbsp;&nbsp;&nbsp;&nbsp;md5sum: 4954d39a72292b0da911d734e1d661cf<br/>
+              &nbsp;&nbsp;&nbsp;&nbsp;size: 1.5 GiB
     </p>
     <p>
         <b>*Please* make sure to read the <a href="<?php echo $ROOT; ?>/Documentation:KXStudio12043:ReleaseNotes">Release Notes</a></b> before using this DVD.<br/>
@@ -113,9 +116,9 @@ include_once("includes/header.php");
         See the <a href="<?php echo $ROOT; ?>/Artwork">KXStudio Artwork</a> section for screenshots of various Desktop Environments using the KXStudio theme.<br/>
     </p>
     <p class="p_extra_spaced">
-        Downloads:<br/>
-        <a href="http://sourceforge.net/projects/kxstudio/files/Releases/artwork/kxstudio-artwork_20121222.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
-            <img src="images/ico_kxstudio.png" alt="" class="img_text_align"/> KXStudio Artwork</a> (version 2012-12-22)
+        <b>Downloads:</b><br/>
+        &nbsp;<a href="http://sourceforge.net/projects/kxstudio/files/Releases/artwork/kxstudio-artwork_20121222.tar.bz2/download" class="external free a_bigger_text" rel="nofollow" target="_blank">
+              <img src="images/ico_kxstudio.png" alt="" class="img_text_align"/> KXStudio Artwork</a> (version 2012-12-22)
     </p>
 </ul>
 
