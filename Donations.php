61e5b30a84684e218e18c19487cf895adb4a9dcd
falkTX <falktx@gmail.com>
2015-07-21 17:31:43 +0200

    Update more sourceforge redirects to linuxaudio

--- Donations.php
+++ Donations.php
@@ -66,7 +66,7 @@ if ($cur_percent > 100.0) {
         }
     } ?>
 </td><td valign="bottom" width="150px">
-    <a class="FlattrButton" style="display:none;" href="http://kxstudio.sourceforge.net/"></a>
+    <a class="FlattrButton" style="display:none;" href="http://kxstudio.linuxaudio.org/"></a>
     <noscript>
         <a href="http://flattr.com/thing/1098067/KXStudio" target="_blank">
         <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>
@@ -76,8 +76,8 @@ if ($cur_percent > 100.0) {
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="A8QZW5UPVZGTW">
         <input type="hidden" name="currency_code" value="EUR">
-        <input type="hidden" name="notify_url" value="http://kxstudio.sourceforge.net/donate/ipn.php"/>
-        <input type="hidden" name="return" value="http://kxstudio.sourceforge.net/donate/thankyou.php"/>
+        <input type="hidden" name="notify_url" value="http://kxstudio.linuxaudio.org/donate/ipn.php"/>
+        <input type="hidden" name="return" value="http://kxstudio.linuxaudio.org/donate/thankyou.php"/>
         <input type="hidden" name="rm" value="2"/>
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal">
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
@@ -98,8 +98,8 @@ if ($cur_percent > 100.0) {
             </td></tr>
         </table>
         <input type="hidden" name="currency_code" value="EUR">
-        <input type="hidden" name="notify_url" value="http://kxstudio.sourceforge.net/donate/ipn.php"/>
-        <input type="hidden" name="return" value="http://kxstudio.sourceforge.net/donate/thankyou.php"/>
+        <input type="hidden" name="notify_url" value="http://kxstudio.linuxaudio.org/donate/ipn.php"/>
+        <input type="hidden" name="return" value="http://kxstudio.linuxaudio.org/donate/thankyou.php"/>
         <input type="hidden" name="rm" value="2"/>
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_LG.gif" border="0" name="submit" alt="PayPal">
         <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
