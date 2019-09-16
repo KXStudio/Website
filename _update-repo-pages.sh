df65bf07a3f09abe71b0f7f2970b1cabfec95e8b
falkTX <falktx@falktx.com>
2019-09-16 13:50:22 +0100

    Make _update-repo-pages.sh safer against failures

--- _update-repo-pages.sh
+++ _update-repo-pages.sh
@@ -7,6 +7,8 @@ set -e
 cd ~/sites/kxstudio
 git pull
 cd repo
-./generate-repo-page.sh apps    > applications.html
-./generate-repo-page.sh plugins > plugins.html
+./generate-repo-page.sh apps    > applications.html.tmp
+./generate-repo-page.sh plugins > plugins.html.tmp
+mv applications.html.tmp applications.html
+mv plugins.html.tmp      plugins.html
 EOF
