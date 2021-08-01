84b81743a7ad476eab7508f865e9ed8e575cd778
falkTX <falktx@falktx.com>
2021-08-01 12:04:39 +0100

    Responsive homepage
    Signed-off-by: falkTX <falktx@falktx.com>

--- index.php
+++ index.php
@@ -15,41 +15,52 @@ include_once("includes/header.php");
     The KXStudio project is divided into 3 main sections:
 </p>
 
-<table>
-    <tr><td width="130" height="130">
-        <p><img src="images/cadence.png" alt="cadence.png" class="img_thumb_128"/></p>
-    </td><td>
+<div class="responsive-item">
+    <div>
+        <a href="<?php echo $ROOT; ?>/Applications">
+            <img src="images/cadence.png" alt="cadence.png" class="img_thumb_128"/>
+        </a>
+    </div>
+    <div>
         <p>
             <b><a href="<?php echo $ROOT; ?>/Applications">Applications</a></b><br/>
             We offer our own custom set of applications and utilities for Linux and Windows.<br/>
             This includes a fully-featured audio plugin host, a JACK patchbay and more.<br/>
             <br/>
         </p>
-    </td></tr>
+    </div>
+</div>
 
-    <tr><td width="130" height="130">
-        <p><img src="images/distrho.png" alt="distrho.png" class="img_thumb_128"/></p>
-    </td><td>
+<div class="responsive-item">
+    <div>
+        <a href="<?php echo $ROOT; ?>/Plugins">
+            <img src="images/distrho.png" alt="distrho.png" class="img_thumb_128"/>
+        </a>
+    </div>
+    <div>
         <p>
             <b><a href="<?php echo $ROOT; ?>/Plugins">Plugins</a></b><br/>
             We provide a wide range of open-source audio plugins and Linux ports.<br/>
             We also provide custom patched plugins to work better and/or support more features.<br/>
         </p>
-    </td></tr>
+    </div>
+</div>
 
-    <tr><td width="130" height="130">
-        <p><img src="images/repos.png" alt="repos.png" class="img_thumb_128"/></p>
-    </td><td>
+<div class="responsive-item last">
+    <div>
+        <a href="<?php echo $ROOT; ?>/Repositories">
+            <img src="images/repos.png" alt="repos.png" class="img_thumb_128"/>
+        </a>
+    </div>
+    <div>
         <p>
             <b><a href="<?php echo $ROOT; ?>/Repositories">Repositories</a></b><br/>
             Debian and Ubuntu compatible repositories are available.<br/>
             Use them to get extra applications and updates, or to upgrade to the KXStudio distribution.<br/>
             <br/>
         </p>
-    </td></tr>
-</table>
-
-<p><br/></p>
+    </div>
+</div>
 
 <hr/>
 
