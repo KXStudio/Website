b8ce1b76b50cd4a6ff224c7149227c275ff48115
falkTX <falktx@falktx.com>
2022-05-22 19:17:17 +0100

    Update repository package entry point
    Signed-off-by: falkTX <falktx@falktx.com>

--- Repositories.php
+++ Repositories.php
@@ -5,7 +5,7 @@ $PAGE_SOURCE_1 = ARRAY("/Repositories");
 $PAGE_SOURCE_2 = ARRAY("Repositories");
 include_once("includes/header.php");
 
-$DEBIAN_PACKAGE     = "kxstudio-repos_11.0.1_all.deb";
+$DEBIAN_PACKAGE     = "kxstudio-repos_11.1.0_all.deb";
 $DEBIAN_PACKAGE_URL = "https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/" . $DEBIAN_PACKAGE;
 ?>
 
