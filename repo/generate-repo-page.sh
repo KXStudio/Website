527e7a03c5cc1e77262fe4e3a54ed8a99cf7a999
falkTX <falktx@falktx.com>
2025-07-24 17:57:19 +0200

    Hide cadence packages, add a few plugins to abandoned list
    Signed-off-by: falkTX <falktx@falktx.com>

--- repo/generate-repo-page.sh
+++ repo/generate-repo-page.sh
@@ -15,13 +15,13 @@ fi
 PACKAGES_ARCHS=("amd64" "arm64" "armhf" "i386")
 
 # software we know to be abandoned, so we give warning to users to maybe not rely on it so much
-PACKAGES_ABANDONED=("cadence" "cadence-tools" "catia" "claudia")
+PACKAGES_ABANDONED=("ams-lv2" "arctican-plugins-lv2" "arctican-plugins-vst" "arpage" "autotalent" "beatslash-lv2" "easyssp-lv2" "easyssp-vst" "invada-studio-plugins-ladspa" "invada-studio-plugins-lv2" "juced-plugins-lv2" "juced-plugins-vst" "pizmidi-plugins" "teragonaudio-plugins-lv2" "teragonaudio-plugins-vst")
 
 # stuff to be hidden, for various reasons
 PACKAGES_BLACKLIST=("carla-bridge-linux32" "carla-bridge-linux64" "distrho-src" "jalv" "lv2vst" "wineasio-amd64")
 
 # hidden packages, either abandoned by the author or stuff I no longer use and thus won't maintain updated here
-PACKAGES_HIDDEN=("fluajho" "gladish" "ladish" "non-sequencer" "non-sequencer-git" "patroneo" "vico")
+PACKAGES_HIDDEN=("cadence" "cadence-tools" "catia" "claudia" "fluajho" "gladish" "ladish" "non-sequencer" "non-sequencer-git" "patroneo" "vico")
 
 # stuff to always be shown, even when their arch is "all"
 PACKAGES_WHITELIST=("cadence" "catia" "claudia" "impro-visor" "j2sc")
